646c0d81e91807f6f57f5ce58fb0f79e95873f5b
Yrlanda <yrlaterivero@gmal.com>
2019-02-07 10:30:22 -0300

    se declara funcion para decodificar letras mayusculas, minusculas, numeros y caracteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -24,7 +24,7 @@ window.cipher = {
 
         //si se cumple la condicion para los números en el código ascii se aplica formula de Michell
         if (ascii_char >= 48 && ascii_char <= 57) {
-          ascii_char = ((ascii_char - 48 + offset) % 10) + 48; 
+          ascii_char = ((ascii_char - 48 + offset) % 10) + 48;
         }
 
         //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
@@ -48,5 +48,58 @@ window.cipher = {
   },
   decode: () => {
     // Acá declaro función para descifrar
+
+
+    function decipher_tex(offset, mensaje) {
+      let decipher_tex = '';
+
+
+      for (let i = 0; i < mensaje.length; i++) {
+
+        let ascii_char = mensaje.charCodeAt(i);
+
+      //si se cumple la condicion para las letras  minuscúlas en el código ascii se aplica formula de Michell
+
+        if (ascii_char >= 97 && ascii_char <= 122) {
+
+          ascii_char = ((ascii_char - 122 - offset) % 26) + 122;
+        }
+
+      //si se cumple la condicion para las letras  mayuscúlas en el código ascii se aplica formula de Michell
+
+        if (ascii_char >= 65 && ascii_char <= 90) {
+          ascii_char = ((ascii_char - 90 - offset) % 26) + 90;
+        }
+
+      //si se cumple la condicion para los números en el código ascii se aplica formula de Michell
+
+        if (ascii_char >= 48 && ascii_char <= 57) {
+          ascii_char = ((ascii_char - 57 - offset) % 10) + 57;
+        }
+
+      //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
+
+        if (ascii_char >= 32 && ascii_char <= 47) {
+          ascii_char = ((ascii_char - 47 - offset) % 16) + 47;
+        }
+
+      //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
+
+        if (ascii_char >= 58 && ascii_char <= 64) {
+          ascii_char = ((ascii_char - 64 - offset) % 7) + 64;
+        }
+
+        //con el metodo de String.fromCharCode obtengo la letra o el caracer del Código Ascii
+
+        decipher_tex = decipher_tex + String.fromCharCode(ascii_char);
+
+      }
+
+      return decipher_tex;
+
+    }
+
+
+
   }
 };
