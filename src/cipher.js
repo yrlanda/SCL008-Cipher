d1b0b4b40efd859acc0b1b6da80df61b76299d48
Yrlanda <yrlaterivero@gmal.com>
2019-02-10 12:41:22 -0300

    se terminan estilos y comentarios de las funciones aplicadas para elementos del DOM

--- src/cipher.js
+++ src/cipher.js
@@ -2,7 +2,6 @@ window.cipher = {
   encode: (offset, mensaje) => {
 
     //aca declaro función para cifrar
-
     let cipher_message = '';
 
     for (let i = 0; i < mensaje.length; i++) {
@@ -12,18 +11,14 @@ window.cipher = {
       if (ascii_char >= 97 && ascii_char <= 122) {
         ascii_char = ((ascii_char - 97 + offset) % 26) + 97;
       }
-
-
       //si se cumple la condicion para las letras  mayuscúlas en el código ascii se aplica formula de Michell
       if (ascii_char >= 65 && ascii_char <= 90) {
         ascii_char = ((ascii_char - 65 + offset) % 26) + 65;
       }
-
       //si se cumple la condicion para los números en el código ascii se aplica formula de Michell
       if (ascii_char >= 48 && ascii_char <= 57) {
         ascii_char = ((ascii_char - 48 + offset) % 10) + 48;
       }
-
       //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
       if (ascii_char >= 32 && ascii_char <= 47) {
         ascii_char = ((ascii_char - 32 + offset) % 16) + 32;
@@ -32,64 +27,60 @@ window.cipher = {
       if (ascii_char >= 58 && ascii_char <= 64) {
         ascii_char = ((ascii_char - 58 + offset) % 7) + 58;
       }
-
-
+      //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
+      if (ascii_char >= 91 && ascii_char <= 96) {
+        ascii_char = ((ascii_char - 91 + offset) % 7) + 91;
+      }
+      //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
+      if (ascii_char >= 123 && ascii_char <= 126) {
+        ascii_char = ((ascii_char - 123 + offset) % 4) + 123;
+      }
+        //con el metodo de String.fromCharCode obtengo la letra o el caracer del Código Ascii
       cipher_message = cipher_message + String.fromCharCode(ascii_char);
-
     }
-
     return cipher_message;
-
   },
+
   decode: (offset, mensaje) => {
     // Acá declaro función para descifrar
-
     let decipher_tex = '';
-
 
     for (let i = 0; i < mensaje.length; i++) {
 
       let ascii_char = mensaje.charCodeAt(i);
 
       //si se cumple la condicion para las letras  minuscúlas en el código ascii se aplica formula de Michell
-
       if (ascii_char >= 97 && ascii_char <= 122) {
-
         ascii_char = ((ascii_char - 122 - offset) % 26) + 122;
       }
-
       //si se cumple la condicion para las letras  mayuscúlas en el código ascii se aplica formula de Michell
-
       if (ascii_char >= 65 && ascii_char <= 90) {
         ascii_char = ((ascii_char - 90 - offset) % 26) + 90;
       }
-
       //si se cumple la condicion para los números en el código ascii se aplica formula de Michell
-
       if (ascii_char >= 48 && ascii_char <= 57) {
         ascii_char = ((ascii_char - 57 - offset) % 10) + 57;
       }
-
       //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
-
       if (ascii_char >= 32 && ascii_char <= 47) {
         ascii_char = ((ascii_char - 47 - offset) % 16) + 47;
       }
-
       //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
-
       if (ascii_char >= 58 && ascii_char <= 64) {
         ascii_char = ((ascii_char - 64 - offset) % 7) + 64;
       }
-
+      //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
+      if (ascii_char >= 91 && ascii_char <= 96) {
+        ascii_char = ((ascii_char - 96 - offset) % 7) + 96;
+      }
+      //si se cumple la condicion para los caracteres especiales en el código ascii se aplica formula de Michell
+      if (ascii_char >= 123 && ascii_char <= 126) {
+        ascii_char = ((ascii_char - 126 - offset) % 4) + 126;
+      }
       //con el metodo de String.fromCharCode obtengo la letra o el caracer del Código Ascii
-
       decipher_tex = decipher_tex + String.fromCharCode(ascii_char);
-
     }
-
     return decipher_tex;
-
   }
-
 }
+
