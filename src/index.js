e2c09aa13552411002ef1b8d2e4cc239c6f32511
Yrlanda <yrlaterivero@gmal.com>
2019-02-09 23:08:31 -0300

    se agregan estilos y funciones del DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 /* Acá va tu código */
+window.onload = () => {
+   
+    const difficulty = document.getElementById("difficulty");
+    const textEncode = document.getElementById("txtEncode");
+    const btn_codificar = document.getElementById("codificar");
+    const btn_decodificar = document.getElementById("decodificar");
+    const text_webcode = document.getElementById("txt_webcode");
+    const btn_clear = document.getElementById("btn_clear");
+     
+
+
+
+    btn_codificar.addEventListener("click", () => {
+        let offset = parseInt(difficulty.value);
+        let text_usuario = textEncode.value;
+        let text_encoded = window.cipher.encode(offset, text_usuario);
+        text_webcode.value = text_encoded; 
+
+    })
+
+    btn_clear.addEventListener("click", () =>{
+        difficulty.value = "";
+        textEncode.value = "";
+        text_webcode.value = "";
+
+
+    })
+
+
+    btn_decodificar.addEventListener("click", () => {
+        let offset = parseInt(difficulty.value);
+        let text_usuario = textEncode.value;
+        let text_decoded = window.cipher.decode(offset, text_usuario);
+        text_webcode.value = text_decoded; 
+
+    })
+
+    
+}
