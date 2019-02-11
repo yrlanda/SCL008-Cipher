75665ff37fb4f0ac21be5ba3710fe9e0653c1ea7
Yrlanda <yrlaterivero@gmal.com>
2019-02-11 18:42:30 -0300

    se realizan pruebas de test para decodificar letras mayusculas, minusculas, numeros y caracteres especiales

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -36,7 +36,23 @@ describe('cipher', () => {
         assert.equal(typeof cipher.decode, 'function');
       });
 
-      it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+      it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+        assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+      });
+
+      it('debería retornar "123478583430" para "890145250107" con offset 7', () => {
+        assert.equal(cipher.decode(7, "890145250107"), "123478583430");
+      });
+
+      it('debería retornar "laboratoria" para "zopcfohcfwo" con offset 14', () => {
+        assert.equal(cipher.decode(14, "zopcfohcfwo"), "laboratoria");
+      });
+
+      it('debería retornar "!$" para "#&" con offset 2', () => {
+        assert.equal(cipher.decode(2, "#&"), "!$");
+      });
+
+
     });
 
   });
